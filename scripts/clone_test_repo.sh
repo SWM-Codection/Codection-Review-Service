f3739283f3c3d7f0955d940a974035bdbe717614
sangcheol12 <eric2057@daum.net>
2024-07-30 23:05:21 +0900

    없을 때만 클론하도록 방식 재변경
    없을 때만 클론하도록 방식 재변경

--- scripts/clone_test_repo.sh
+++ scripts/clone_test_repo.sh
@@ -1,9 +1,7 @@
 DEST_DIR="$HOME/codection/test"
 
-# 폴더가 이미 존재하면 삭제
-if [ -d "$DEST_DIR" ]; then
-    rm -rf "$DEST_DIR"
+# 폴더가 존재하지 않으면 생성하고 클론
+if [ ! -d "$DEST_DIR" ]; then
+    mkdir -p "$DEST_DIR"
+    git clone https://github.com/SWM-Codection/Git-Test.git "$DEST_DIR"
 fi
-
-mkdir -p "$DEST_DIR"
-git clone https://github.com/SWM-Codection/Git-Test.git "$DEST_DIR"
