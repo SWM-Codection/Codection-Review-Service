74e58381e5b664e198f7c7bb6ddd797dc093b137
cloudchamb3r <jizon0123@protonmail.com>
2024-10-02 15:13:19 +0900

    feat: add ReactionTypeEnumSerializer

--- src/main/kotlin/swm/virtuoso/reviewservice/domain/DiscussionReaction.kt
+++ src/main/kotlin/swm/virtuoso/reviewservice/domain/DiscussionReaction.kt
@@ -1,11 +1,14 @@
 package swm.virtuoso.reviewservice.domain
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import swm.virtuoso.reviewservice.adapter.`in`.web.dto.request.ReactionRequest
 import swm.virtuoso.reviewservice.adapter.out.persistence.entity.discussion.DiscussionReactionEntity
 import swm.virtuoso.reviewservice.common.enums.ReactionTypeEnum
+import swm.virtuoso.reviewservice.common.enums.ReactionTypeEnumSerializer
 
 data class DiscussionReaction(
     val id: Long? = null,
+    @JsonSerialize(using = ReactionTypeEnumSerializer::class)
     val type: ReactionTypeEnum,
     val discussionId: Long,
     val commentId: Long? = null,
