ae7b76076b7a816fea38f05b70e8668bb502aa5b
Lieslot <rlaxodud1999@gmail.com>
2024-10-19 15:02:15 +0900

    feat: discussion reaction 엔드포인트 추가

--- src/main/kotlin/swm/virtuoso/reviewservice/adapter/in/web/controller/DiscussionDetailController.kt
+++ src/main/kotlin/swm/virtuoso/reviewservice/adapter/in/web/controller/DiscussionDetailController.kt
@@ -8,15 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import swm.virtuoso.reviewservice.adapter.`in`.web.dto.request.ChangeDiscussionWatchRequest
 import swm.virtuoso.reviewservice.adapter.`in`.web.dto.request.DeleteReactionRequest
 import swm.virtuoso.reviewservice.adapter.`in`.web.dto.request.DiscussionWatchRequest
@@ -67,6 +59,30 @@ class DiscussionDetailController(
     ): DiscussionContentResponse {
         return discussionFileUseCase.getDiscussionContents(discussionId)
     }
+
+    @GetMapping("/reaction")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "give reaction", description = "게시글 혹은 코멘트에 반응 추가")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "204",
+                description = "반응 추가 성공",
+                content = [Content(schema = Schema(implementation = Long::class))]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "디스커션 혹은 코멘트 정보를 찾을 수 없음",
+                content = [Content(schema = Schema(implementation = ErrorResponse::class))]
+            )
+        ]
+    )
+    fun getReaction(
+        @RequestParam commentId: Long
+    ): List<DiscussionReaction> {
+        return discussionReactionUseCase.getDiscussionCommentReactions(commentId)
+    }
+
 
     @PostMapping("/reaction")
     @ResponseStatus(HttpStatus.CREATED)
